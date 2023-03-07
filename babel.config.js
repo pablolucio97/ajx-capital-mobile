cf41e1a742fe4f4c46119ad7513827c6e400d4bc
pablolucio97 <pablolucio_@hotmail.com>
2023-03-07 14:19:18 -0300

    chore: added configuration to use absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
+// eslint-disable-next-line no-undef
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            assets: './assets',
+            components: './src/components',
+            screens: './src/screens',
+            themes: './src/themes',
+            services: './src/services',
+            data: './src/data',
+            hooks: './src/hooks',
+            contexts: './src/contexts',
+            utils: './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
